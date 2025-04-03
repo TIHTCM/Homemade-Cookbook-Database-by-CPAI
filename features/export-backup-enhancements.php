e7f23493d3dfe1648f098f4035356a7c3d594e80
TIHTCM <thisishowtocontactme@gmail.com>
2025-04-03 14:23:54 -0400

    Create export-backup-enhancements.php

---
+++ features/export-backup-enhancements.php
@@ -0,0 +1,13 @@
+/**
+ * Exports plugin data as JSON files for backup.
+ */
+function hcdb_export_data() {
+    // Logic for exporting data
+}
+
+/**
+ * Imports plugin data from JSON files.
+ */
+function hcdb_process_import_data() {
+    // Logic for importing data
+}
